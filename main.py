78adc45a335028c92efda4d11435507f47339acf
Rich Jolissaint <rich@risingfruition.com>
2024-10-09 21:26:27 -0700

    R Put main code into function 'main'.

--- main.py
+++ main.py
@@ -9,21 +9,26 @@ class Helper:
         self.words = filter_words(words, self.guess, self.score)
 
 
-secret_words = []
-with open("secret_words.csv", encoding="utf-8") as f:
-    word = f.readline()
-    while word:
-        secret_words.append(word.strip())
+def main():
+    secret_words = []
+    with open("secret_words.csv", encoding="utf-8") as f:
         word = f.readline()
+        while word:
+            secret_words.append(word.strip())
+            word = f.readline()
 
-for i, w in enumerate(secret_words):
-    print(i + 1, w)
-
-words = secret_words
-for count in range(6):
-    guess = input("Input guess : ")
-    result = input("Input result: ")
-    helper = Helper(guess, result, words)
-    words = helper.words
-    for i, w in enumerate(words):
+    for i, w in enumerate(secret_words):
         print(i + 1, w)
+
+    words = secret_words
+    for count in range(6):
+        guess = input("Input guess : ")
+        result = input("Input result: ")
+        helper = Helper(guess, result, words)
+        words = helper.words
+        for i, w in enumerate(words):
+            print(i + 1, w)
+
+
+if __name__ == "__main__":
+    main()
